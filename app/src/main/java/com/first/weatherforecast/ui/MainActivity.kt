958a0cfa6ccef083dc2ff609c1ab27e5e847eb5e
AnnaG <anutagoroxowa@yandex.ru>
2021-08-25 19:50:08 +0300

    Add: fun handleWeatherResponse

--- app/src/main/java/com/first/weatherforecast/ui/MainActivity.kt
+++ app/src/main/java/com/first/weatherforecast/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.first.weatherforecast.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -25,20 +26,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<Weather>, response: Response<Weather>) {
                 val weather: Weather = response.body() ?: return
-                val town: TextView = findViewById(R.id.town)
-                val degrees: TextView = findViewById(R.id.degrees)
-                val maxMin: TextView = findViewById(R.id.minMax)
-
-                val skyCondition: TextView = findViewById(R.id.skyCondition)
-                val skyImage: ImageView = findViewById(R.id.SkyImage)
-
-                town.text = weather.city
-
-                degrees.text = weather.degree.toString() + "°C"
-                maxMin.text = "${weather.tempMax}/${weather.tempMin}"
-
-                skyCondition.setText(weather.skyCondition.text)
-                skyImage.setImageResource(weather.skyImage.image)
+                handleWeatherResponse(weather)
             }
 
             override fun onFailure(call: Call<Weather>, t: Throwable) {
@@ -49,5 +37,24 @@ class MainActivity : AppCompatActivity() {
         NetworkManager.api()
             ?.getWeather()
             ?.enqueue(request)
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun handleWeatherResponse(weather: Weather) {
+        val town: TextView = findViewById(R.id.town)
+        val degrees: TextView = findViewById(R.id.degrees)
+        val maxMin: TextView = findViewById(R.id.minMax)
+
+        val skyCondition: TextView = findViewById(R.id.skyCondition)
+        val skyImage: ImageView = findViewById(R.id.SkyImage)
+
+        town.text = weather.city
+
+        degrees.text = weather.degree.toString() + "°C"
+        maxMin.text = "${weather.tempMax}/${weather.tempMin}"
+
+        skyCondition.setText(weather.skyCondition.text)
+        skyImage.setImageResource(weather.skyImage.image)
     }
 }
