36f3bdcab42c6f29567bd3471ecdabb59db640fe
AnnaG <anutagoroxowa@yandex.ru>
2021-09-14 18:06:30 +0300

    Add: Activity for dialog

--- app/src/main/java/com/first/weatherforecast/ui/CityDialog.kt
+++ app/src/main/java/com/first/weatherforecast/ui/CityDialog.kt
@@ -4,11 +4,17 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import com.first.weatherforecast.R
 
 class CityDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreateDialog(savedInstanceState)
         val builder = AlertDialog.Builder(activity)
-        return builder.setTitle("Диалоговое окно").setMessage("Для закрытия нажмите ОК").create()
+        return builder
+            .setTitle("Введите координаты города")
+            .setView(R.layout.fragment_cities_dialog)
+            .setPositiveButton("Добавить", null)
+            .setNegativeButton("Отмена", null)
+            .create()
     }
 }
