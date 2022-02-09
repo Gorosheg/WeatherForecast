0386d64c88ea9f753af4016bbd27b99aaf82697c
AnnaG <anutagoroxowa@yandex.ru>
2022-02-09 16:32:45 +0300

    Add: Swipe to delete to left and right sides

--- citiesScreen/src/main/java/com/first/citiesscreen/presentation/recycler/CitiesItemTouchHelper.kt
+++ citiesScreen/src/main/java/com/first/citiesscreen/presentation/recycler/CitiesItemTouchHelper.kt
@@ -6,10 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 internal class CitiesItemTouchHelper(
     private val swipe: (Int) -> Unit
 ) :
-    ItemTouchHelper.SimpleCallback(
-        0,
-        ItemTouchHelper.LEFT
-    ) {
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
